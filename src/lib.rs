a4fb7104e8e4967d4f51edd8da261cf9841af2d0
Sarvil Rathour <ken.kaneki.260905@gmail.com>
2026-07-06 19:50:59 +0530

    feat: thread spawning

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,23 @@
-pub struct ThreadPool;
-impl ThreadPool{
-    pub fn new()->Self{
-        Self
+pub struct ThreadPool {
+    handles: Vec<std::thread::JoinHandle<()>>,
+}
+impl ThreadPool {
+    pub fn new(threads_count: u8) -> Self {
+        let handles=(0..threads_count).map(|_|{
+            std::thread::spawn(||{})
+        }).collect();
+        Self { handles }
     }
-    pub fn execute<T:Fn()>(&self,work:T){
-        
-    }
+    pub fn execute<T: Fn()>(&self, work: T) {}
 }
 
-
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
     #[test]
-    fn it_works(){
-        let pool=ThreadPool::new();
-        pool.execute(||{println!("Hello from the 1st thread")});
-        pool.execute(||{println!("Hello from the 2nd thread")});
+    fn it_works() {
+        let pool = ThreadPool::new(10);
+        pool.execute(|| println!("Hello from the 1st thread"));
+        pool.execute(|| println!("Hello from the 2nd thread"));
     }
 }
